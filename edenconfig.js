117130436104befaa91f0050759097b8aeb54c1a
installgen2 <gen2@gen2.space>
2019-01-16 17:42:53 -0700

    include own config

---
+++ edenconfig.js
@@ -0,0 +1,14 @@
+const config = {};
+
+config.sass = [
+  './node_modules/bootstrap/scss/bootstrap.scss',
+];
+
+config.js = [
+  './node_modules/whatwg-fetch/dist/fetch.umd.js',
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/popper.js/dist/umd/popper.min.js',
+  './node_modules/bootstrap/dist/js/bootstrap.js',
+];
+
+module.exports = config;
