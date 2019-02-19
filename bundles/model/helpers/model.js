3618eecc4edffb8a8bc1cadc536b9bf5387d064a
EdenCoder <alex@adlist.co.nz>
2019-02-19 14:45:38 +1000

    amend model listener, upgrade dependencies, bump version

--- bundles/model/helpers/model.js
+++ bundles/model/helpers/model.js
@@ -24,29 +24,35 @@ class ModelHelper extends helper {
    * Live listens to model
    *
    * @param  {String}  sessionID
-   * @param  {*}       Model
+   * @param  {*}       listenModel
    * @param  {String}  listenID
    *
    * @returns {Promise}
    */
-  deafen(sessionID, Model, listenID) {
+  deafen(sessionID, listenModel, listenID) {
+    // check model
+    if (!listenModel || !listenModel.get('_id')) return;
+
     // Call local
-    return this.eden.thread('compute', 0).call('model.deafen', sessionID, Model.constructor.name.toLowerCase(), Model.get('_id').toString(), listenID);
+    return this.eden.thread('compute', 0).call('model.deafen', sessionID, listenModel.constructor.name.toLowerCase(), listenModel.get('_id').toString(), listenID);
   }
 
   /**
    * Live listens to model
    *
    * @param  {String}  sessionID
-   * @param  {*}       Model
+   * @param  {*}       listenModel
    * @param  {String}  listenID
    * @param  {Boolean} atomic
    *
    * @returns {Promise}
    */
-  listen(sessionID, Model, listenID, atomic = false) {
+  listen(sessionID, listenModel, listenID, atomic = false) {
+    // check model
+    if (!listenModel || !listenModel.get('_id')) return;
+
     // Call local
-    return this.eden.thread('compute', 0).call('model.listen', sessionID, Model.constructor.name.toLowerCase(), Model.get('_id').toString(), listenID, atomic);
+    return this.eden.thread('compute', 0).call('model.listen', sessionID, listenModel.constructor.name.toLowerCase(), listenModel.get('_id').toString(), listenID, atomic);
   }
 }
 
